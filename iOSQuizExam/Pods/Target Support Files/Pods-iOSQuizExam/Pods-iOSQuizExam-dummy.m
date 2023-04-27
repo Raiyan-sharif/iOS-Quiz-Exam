8052e6a69df21be3d109ccf019444088a565ed4b
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-04-27 16:19:38 +0600

    init

---
+++ iOSQuizExam/Pods/Target Support Files/Pods-iOSQuizExam/Pods-iOSQuizExam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSQuizExam : NSObject
+@end
+@implementation PodsDummy_Pods_iOSQuizExam
+@end
---
+++ iOSQuizExam/iOSQuizExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSQuizExam
+//
+//  Created by BJIT on 27/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
